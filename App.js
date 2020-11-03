12914ad58a6962dfd411e17517a5c51732e0bd0c
Palloma Gualter <pallomagualter@gmail.com>
2020-11-03 10:10:42 -0300

    login

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, TouchableOpacity, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá bebê!</Text>
       <StatusBar style="auto" />
     </View>
   );
