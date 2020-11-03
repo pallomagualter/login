2d99f9d49ff35b1ad8b56ac9fe8305c199b56b92
Palloma Gualter <pallomagualter@gmail.com>
2020-11-03 10:22:33 -0300

    login

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, TextInput, View, TouchableOpacity, Text } from 'react-native';
 
 export default function App() {
+  state = {
+    email: '',
+    password: '',
+  }
+
   return (
     <View style={styles.container}>
       <Text>Olá bebê!</Text>
-      <StatusBar style="auto" />
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Digite seu e-mail"
+        value={this.state.email}
+        onChangeText={email => this.setState({ email })}
+      />
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Digite sua senha"
+        value={this.state.senha}
+        onChangeText={senha => this.setState({ senha })}
+      />
+      
     </View>
   );
 }
